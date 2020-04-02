bb5daf30100593e087b4db24edef7f9d1775f305
Daniel Gamarro <Daniel Gamarro@gampc_01>
2020-04-02 17:01:18 -0600

    Segunda Modificacion

---
+++ Prueba30Marzoo/src/main/java/prueba30marzoo/Prueba30Marzo.java
@@ -0,0 +1,20 @@
+
+package prueba30marzoo;
+
+/**
+ *
+ * @author Daniel Gamarro
+ */
+public class Prueba30Marzo {
+    
+public static void main(String[]args){
+    System.out.println("Julio Daniel Gamarro Cano");
+    System.out.println("1ra. Ave. 1-22 zona 1 Huehuetenango");
+    System.out.println("59032907");
+    
+}
+    
+}
+
+    
+}
